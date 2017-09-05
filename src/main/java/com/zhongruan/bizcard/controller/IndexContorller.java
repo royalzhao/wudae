c355dfb66b82f391b2759b2f69d0a768ebb69556
royalzhao <royal_zhaoshuai@163.com>
2017-09-05 09:28:13 +0800

    更新

--- src/main/java/com/zhongruan/bizcard/controller/IndexContorller.java
+++ src/main/java/com/zhongruan/bizcard/controller/IndexContorller.java
@@ -7,17 +7,20 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
 
 import com.zhongruan.bizcard.entity.ContactEntity;
 import com.zhongruan.bizcard.service.ContactService;
@@ -26,134 +29,64 @@ import com.zhongruan.bizcard.service.ContactService;
 public class IndexContorller {
 
 	private final Logger logger = LoggerFactory.getLogger(IndexContorller.class);
-	@RequestMapping(value="/")
-	public String index(){
+	@Autowired
+	ContactService service;
+	//��ҳ
+	@RequestMapping(value="index")
+	public String index() {
 		return "index";
 	}
-	@RequestMapping(value="/login")
-	public String login(){
+	//��½
+	@RequestMapping(value="/")
+	public String login() {
 		return "login";
 	}
-
-	@RequestMapping(value="/content")
-	public String content(){
-		return "content";
-	}
-
-
-	//��ѯ
-	@Autowired
-	ContactService contactservice;
-	@RequestMapping(value="/index")
-	//@PostMapping("quancha1")
-	public String select(Map m){
-		List<ContactEntity> list= contactservice.findAll();
-		m.put("list", list);
-		return "index";
-	}
-
-	@PostMapping("/")
+	//��½��֤
+	@PostMapping(value="Login")
 	@ResponseBody
-	public List<ContactEntity> indexPost(
-			@RequestParam(value = "keyword", defaultValue = "") String keyword) {
-		logger.info("{}", keyword);
-		List<ContactEntity> contacts = contactservice.findByName(keyword);
-		logger.info("{}", contacts);
-		return contacts;
-	}
-
-
-	//����
-	@PostMapping("add2")
-	//@GetMapping("add2")
-	@ResponseBody
-	public String add1(
-			@RequestParam(value="sid",defaultValue="") String sid,
-			@RequestParam(value="sname",defaultValue="")String sname,
-			@RequestParam(value="age",defaultValue="")String age
-			) {
-		System.out.print("----------------"+sid+sname+age);
-		contactservice.add(sid,sname,age);
-		return "ok";
-	}
-
-
-	//ɾ��
-	@GetMapping("/shan")
-	public String shan(
-			@RequestParam(value="sid",defaultValue="")String sid
-			){
-		contactservice.shan(sid);
-		return "redirect:/index";
-	}
-	//��¼��֤
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	@ResponseBody
-	public Map<String,String> loginPost(
-			@RequestParam(value="sid",defaultValue="")String sid,
-			@RequestParam(value="age",defaultValue="")String age,
+	public Map<String, String> Login(
+			@RequestParam(value="account",defaultValue="") String user_name,
+			@RequestParam(value="password",defaultValue="") String user_password,
 			HttpServletRequest request
 			) {
-
-		HttpSession session = request.getSession();
-		Map<String, Object> result = contactservice.login(sid,age);
-		Map<String, String> map = new HashMap();
-		if (!"1".equals(result.get("counter").toString())) {
-			map.put("message", "ERROR");
+		HttpSession session= request.getSession();
+		Map<String, Object> result=service.userlogin(user_name, user_password);
+		Map<String, String> map  = new HashMap();
+		
+		if(!"1".equals(result.get("counter").toString())) {
+			map.put("message","error");
 			return map;
 		}
 		session.setAttribute("result", result);
-		map.put("message", "OK"); 
-		// ��id�����ݱ��浽Session��Cookie�򷵻�token
-		// redirect: ������ת������ҳ��
+		map.put("message", "ok");
 		return map;
 	}
-
-
-	/*
-@PostMapping("/login")
-public Map<String, String> login1(
-		HttpServletRequest request,
-		@SessionAttribute ContactEntity uesr
-		) {
-		HttpSession session = request.getSession();
-		Map<String,Object> user = contactservice.login(user);
-		session.setAttribute("user",user);
-		 Map<String, String> map = new HashMap();
-
-		    // �ж�counter�����������������һ��˵��û��������˺Ż��������쳣
-		    if (!"1".equals(user.get("counter").toString())) {
-		      // ���ݳ����ض��򵽵�¼��url
-		      map.put("message", "ERROR");
-		      return map;
-		    }
-		    map.put("message", "OK");
-		    // ��id�����ݱ��浽Session��Cookie�򷵻�token
-		    // redirect: ������ת������ҳ��
-		    return map;
-		  }
-	 */
-
-	@PostMapping("/check")
+	//����
+	@PostMapping(value="uname")
 	@ResponseBody
-	public Map<String, String> check(
-			@RequestParam(value = "qqq", defaultValue = "") String qqq
-			) {
-		// logger.info("{}", account);
-		System.out.println(qqq);
-		Map<String,Object> user = contactservice.yanzheng(qqq);
+	public Map<String, String> uname(@RequestParam(value="account",defaultValue="") String user_name,HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Map<String,Object> result = service.uname(user_name);
 		Map<String,String> map = new HashMap();
-		 if (!"1".equals(user.get("counter").toString())) {
-		      // ���ݳ����ض��򵽵�¼��url
-		      map.put("message", "OK");
-		      return map;
-		    }
-		  map.put("message", "ELL");
-		    // ��id�����ݱ��浽Session��Cookie�򷵻�token
-		    // redirect: ������ת������ҳ��
-		    return map;
+		if(!"1".equals(result.get("counter").toString())) {
+			map.put("messages", "error");
+			System.out.println("qqq"+map);
+			return map;
+		}
+		session.setAttribute("result", result);
+		map.put("messages", "ok");
+		System.out.println("ssss"+map);
+		return map;
 	}
-	 
+	//ע��
+	@PostMapping(value="add")
+	@ResponseBody
+	public String add(@RequestParam(value="account",defaultValue="") String user_name,
+			@RequestParam(value="password",defaultValue="") String user_password) {
+		System.out.println("-------"+user_name);
+		service.add(user_name, user_password);
+		return "index";
+	}
 
 	
 }
